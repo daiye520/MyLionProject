392b9cae00e3b6c6cbd68a6beae09a7ec98226f3
taino <tai.p03936@jsol.co.jp>
2020-07-20 05:33:28 +0000

    初めてのバージョン

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("YTAI_PG10.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
