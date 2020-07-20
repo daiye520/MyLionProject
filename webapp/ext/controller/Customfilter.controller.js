5df12a8accbfa0dd0298d56cc7a2a143e0d34617
taino <tai.p03936@jsol.co.jp>
2020-07-20 05:33:58 +0000

    初めてのバージョン

---
+++ webapp/ext/controller/Customfilter.controller.js
@@ -0,0 +1,7 @@
+sap.ui.controller("YTAI_PG10.ext.controller.CustomFilter", {
+		onBeforeRebindTableExtension: function(oEvent) {
+		var oID = oEvent.getSource().getId();
+	  	// to select only one specific table
+	  	//alert(‘onBeforeRebindTableExtension called!’);
+		}
+});
